2ed2a6c44c92ef17c5ac4c7900da3de5570524ae
Aleksey_Milyaev <Trane94.milaev@yandex.ru>
2024-10-09 18:18:07 +0700

    finish project

--- main.py
+++ main.py
@@ -1,35 +1,96 @@
-# Создание экземпляра класса для работы с API сайтов с вакансиями
-hh_api = HeadHunterAPI()
+from src.head_hunter_api import HeadHunterAPI
+from src.saver import JSONSaver
+from src.utils import get_top_vacancies, get_vacancies_by_salary_from
+from src.vacancy import Vacancy
 
-# Получение вакансий с hh.ru в формате JSON
-hh_vacancies = hh_api.get_vacancies("Python")
 
-# Преобразование набора данных из JSON в список объектов
-vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
-
-# Пример работы контструктора класса с одной вакансией
-vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
-
-# Сохранение информации о вакансиях в файл
-json_saver = JSONSaver()
-json_saver.add_vacancy(vacancy)
-json_saver.delete_vacancy(vacancy)
-
-# Функция для взаимодействия с пользователем
-def user_interaction():
-    platforms = ["HeadHunter"]
+def user_interaction() -> None:
+    """Функция для взаимодействия с пользователем"""
     search_query = input("Введите поисковый запрос: ")
-    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    salary_range = input("Введите диапазон зарплат: ") # Пример: 100000 - 150000
+    print(f"Пожалуйста, подождите. Идёт поиск вакансий по запросу <{search_query}>...\n")
 
-    filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
+    # поиск вакансий по запросу на hh.ru
+    hh_api = HeadHunterAPI()
+    hh_vacancies = hh_api.load_vacancies(search_query)
 
-    ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
+    # сохранение полученных вакансий в json-файл
+    saver = JSONSaver()
+    saver.add_vacancies(hh_vacancies)
 
-    sorted_vacancies = sort_vacancies(ranged_vacancies)
-    top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    print_vacancies(top_vacancies)
+    # выбор пользователем действия
+    if hh_vacancies:
+        print("Выберите действие:")
+        print("1. Показать все вакансии")
+        print("2. Найти вакансии по названию")
+        print("3. Показать вакансии от указанной зарплаты")
+        print("4. Показать топ N вакансий зарплате")
+        print("5. Добавить вакансию")
+        print("6. Удалить вакансию по url\n")
+
+        # проверка корректности ввода пользователя
+        try:
+            action = int(input("Введите цифру от 1 до 6: "))
+        except ValueError:
+            print("Некорректный ввод. Выбрано действие 1")
+            action = 1
+        else:
+            if action not in range(1, 7):
+                print("Некорректный ввод. Выбрано действие 1")
+                action = 1
+
+    if action == 1:
+        for vac in Vacancy.cast_to_object_list(hh_vacancies):
+            print(vac)
+            print()
+
+    elif action == 2:
+        vacancy_name = input("Введите слово для поиска: ").lower()
+        for vac in saver.get_vacancy_by_vacancy_name(vacancy_name):
+            print(vac)
+            print()
+
+    elif action == 3:
+        try:
+            salary_from = int(input("Укажите нижний порог зарплаты (целое число от 0): "))
+        except ValueError:
+            print("Некорректный ввод. Нижний порог не указан")
+            salary_from = 0
+        vacs_objects = Vacancy.cast_to_object_list(hh_vacancies)
+        for vac in get_vacancies_by_salary_from(vacs_objects, salary_from):
+            print(vac)
+            print()
+
+    elif action == 4:
+        try:
+            top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+        except ValueError:
+            print("Количество вакансий должно быть целым числом")
+            print("По умолчанию будет выведено 10 вакансий\n")
+            top_n = 10
+
+        if top_n > len(hh_vacancies):
+            top_n = len(hh_vacancies) - 1
+
+        vacs_objects = Vacancy.cast_to_object_list(hh_vacancies)
+        for vac in get_top_vacancies(vacs_objects, top_n):
+            print(vac)
+            print()
+
+    elif action == 5:
+        vacancy = Vacancy(
+            input("Введите название: "),
+            input("Введите ссылку на вакансию: "),
+            input("Введите требования к работе: "),
+            input("Введите рабочие обязанности: "),
+            int(input("Введите зарплату: ")),
+        )
+        saver.add_vacancy(vacancy)
+
+    elif action == 6:
+        url = input("Введите ссылку на вакансию: ")
+        saver.del_vacancy(url)
+
+    print("Работа программы завершена")
 
 
 if __name__ == "__main__":
--- src/еуые.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-url = "https://api.hh.ru/vacancies"
-params = {'text': "python", 'page': 1, 'per_page': 100}
-
-response = requests.get(url, params=params)
-print(len(json.loads(response.text)["items"]))
